96adcecc769e36ae907ad562df8810cea48ee342
Andrii <andrii@srsolutions.biz>
2019-09-03 23:13:42 +0300

    formats added

--- src/app/scanner/scanner.component.ts
+++ src/app/scanner/scanner.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BarcodeFormat } from '@zxing/library';
+
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.component.html',
   styleUrls: ['./scanner.component.scss'],
 })
 export class ScannerComponent implements OnInit {
-  scanned;
+  scanned: string;
+  formats = [
+    BarcodeFormat.QR_CODE,
+    BarcodeFormat.EAN_13,
+    BarcodeFormat.CODE_128,
+  ];
+
   constructor() {}
 
   ngOnInit() {}
 
-  onScan(scan): void {
-    console.log(scan);
-    this.scanned = scan;
+  onScan(scanned: string): void {
+    this.scanned = scanned;
   }
 }
