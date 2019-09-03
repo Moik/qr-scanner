66d79ecf175acc876cdad5aa75f8a579b7449433
Andrii <andrii@srsolutions.biz>
2019-09-03 22:22:19 +0300

    routes added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { ScannerComponent } from './scanner/scanner.component';
+
+const routes: Routes = [
+  { path: 'scanner', component: ScannerComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScannerComponent } from './scanner/scanner.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, ZXingScannerModule],
+  declarations: [AppComponent, ScannerComponent, HomeComponent],
+  imports: [BrowserModule, ZXingScannerModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
